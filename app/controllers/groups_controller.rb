eaac833d0da80ee381ac38b97f39189e9358fd3b
lancaoxiangshe <wulantoo@live.com>
2016-12-23 09:53:53 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,10 +12,10 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
-     find_group_and_check_permission
   end
 
   def create
@@ -30,7 +30,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-     find_group_and_check_permission
 
     if @group.update(group_params)
     redirect_to groups_path, notice: "Update Success"
@@ -40,7 +39,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-     find_group_and_check_permission
 
     @group.destroy
     flash[:alert] = "Group deleted"
